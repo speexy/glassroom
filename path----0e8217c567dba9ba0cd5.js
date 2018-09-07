d8501e9deed659b91e94198f26f374877908e03a
speexy <jennifer.meyer@posteo.de>
2018-09-08 01:52:53 +0200

    Updates

---
+++ path----0e8217c567dba9ba0cd5.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{107:function(t,e){t.exports={data:{allPages:{edges:[{node:{fields:{slug:"/navigation"},frontmatter:{title:""}}},{node:{fields:{slug:"/blog"},frontmatter:{title:"blog"}}},{node:{fields:{slug:"/glassroom-experience"},frontmatter:{title:""}}},{node:{fields:{slug:"/about"},frontmatter:{title:""}}},{node:{fields:{slug:"/"},frontmatter:{title:""}}}]}},layoutContext:{}}}});
+//# sourceMappingURL=path----0e8217c567dba9ba0cd5.js.map
---
+++ path---blog-dacc965bad45476f61ce.js
@@ -0,0 +1,2 @@
+webpackJsonp([49683490770531],{352:function(t,o){t.exports={data:{markdownRemark:{frontmatter:{title:"blog"}}},pathContext:{slug:"/blog"}}}});
+//# sourceMappingURL=path---blog-dacc965bad45476f61ce.js.map
