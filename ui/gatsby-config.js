2037196b4bb051c88532b7e24257564a4a0d7360
speexy <jennifer.meyer@posteo.de>
2018-09-07 06:22:48 +0200

    gh pages plugin

--- ui/gatsby-config.js
+++ ui/gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
         title: '',
         description: ''
     },
-    pathPrefix: '/',
+    pathPrefix: '/glassroom',
     plugins: [
         'gatsby-plugin-react-helmet',
         {
