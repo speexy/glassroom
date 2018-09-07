03851c2f7576963bbc859f4af18ff1fe11990ebb
speexy <jennifer.meyer@posteo.de>
2018-09-07 02:21:35 +0200

    first commit

---
+++ ui/.cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/home/jenn/n3xtcoder/glassroom/ui/src/layouts/index.js"
+  import data from "/home/jenn/n3xtcoder/glassroom/ui/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
