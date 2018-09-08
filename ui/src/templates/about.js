d04d18e147f743e036f314288e2035f9014c3420
speexy <jennifer.meyer@posteo.de>
2018-09-08 05:04:11 +0200

    curators

--- ui/src/templates/about.js
+++ ui/src/templates/about.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import {Blockquote} from '../components/Blockquote.js';
 import {ImageRow} from '../components/ImageRow.js';
 import {Content} from '../components/Content.js';
@@ -6,7 +7,7 @@ import styles from './about.module.css';
 
 export default ({ data }) => {
 
-  const {blockquote, imageRow } = data.about.frontmatter
+  const {blockquote, imageRow, curators } = data.about.frontmatter
 
     return (
         <div>
@@ -16,6 +17,19 @@ export default ({ data }) => {
           <Blockquote quote={blockquote}/>
           <ImageRow images={imageRow}/>
           <Content content={data.about.html }/>
+
+          <div className={styles.greyBackground}>
+
+            <div className={styles.curators}>
+              <h2>Curators</h2>
+              <div className={styles.curatorWrapper}>
+                <div className={styles.curatorImage} style={{ backgroundImage: `url(${curators.image})` }}></div>
+                <ReactMarkdown source={curators.text}/>
+              </div>
+            </div>
+
+          </div>
+
         </div>
     );
 
@@ -28,6 +42,10 @@ export const aboutQuery = graphql`
       frontmatter {
         blockquote
         imageRow
+        curators {
+          image
+          text
+        }
       }
       html
     }
